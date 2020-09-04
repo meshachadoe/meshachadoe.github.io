61b9ca45bffb6b47526a410b4e7c79fa53765510
meshachadoe <marlienri.avehas@gmail.com>
2020-09-04 14:08:00 +0700

    Revert home page and added project display page

---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+let DOMelements = {
+    menuToggle: '.nav-toggle'
+};
+
+document.querySelector(DOMelements.menuToggle).addEventListener("click", function() {
+    console.log("clicked");
+    
+})
